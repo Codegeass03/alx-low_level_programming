082d1c57ff42dc65ecbf973d65b6e4f62b651ec1
Codegeass03 <vivanchepchumba32@gmail.com>
2023-08-21 11:55:02 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
-#include <limits.h>
-
+#include "main.h"
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
@@ -8,27 +7,38 @@
  */
 int _atoi(char *s)
 {
-int sign = 1;
-int result = 0;
-int = 0;
-while (s[i] == ' ' || s[i] == '\t')
-{
-i++;
-}
-if (s[i] == '-' || s[i] == '+')
-{
-sign = (s[i] == '-') ? -1 : 1;
-i++;
-}
-(s[i] >= '0' && s[i] <= '9')
-{
-if (result > INT_MAX / 10 ||
-(result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
-{
-return (sign == 1)
-}
-result = result * 10 + (s[i] - '0');
-i++;
-}
-return (result *sign)
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
