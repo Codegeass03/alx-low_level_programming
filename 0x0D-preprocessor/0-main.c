d5a9fa6ba4179c83161fbbf9d3f0e74c0486b5ef
Codegeass03 <vivianchepchumba32@gmail.com>
2023-09-11 12:30:41 -0700

    C - Preprocessor

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -6,12 +6,14 @@
  *  * main - check the code
  *   *
  *    * Return: Always 0.
- *     */
+ *
+ *
+ */
 int main(void)
 {
 	    int s;
 
-	        s = 98 + SIZE;
-		    printf("%d\n", s);
-		        return (0);
+	    s = 98 + SIZE;
+	    printf("%d\n", s);
+return (0);
 }
