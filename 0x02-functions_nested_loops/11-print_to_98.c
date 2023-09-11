76b1905e52393efdb9d9dcc04da775d260ac12db
Codegeass03 <vivianchepchumba32@gmail.com>
2023-09-11 08:37:25 -0700

    Object-like Macro

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - prints all natural numbers from n to 98;
+ * print_to_98 - prints all natural numbers from n to 98
  *
  * @n: starting point
  *
- * Return: returns nothing;
+ * Return: returns nothing
  *
  */
 void print_to_98(int n)
