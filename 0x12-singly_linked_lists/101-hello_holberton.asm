36f7114f24fdbaecaf921414be58502c79100515
Codegeass03 <vivianchepchumba32@gmail.com>
2023-09-21 12:04:11 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,10 @@
-section .data
-    format db "Hello, Holberton\n",0
-
 section .text
-    global main
-    extern printf
-
-main: ;Calling printf
-mov rdi, format  ; Load the address of the format string into rdi (1st argument)
-xor rax, rax    ; Clear rax (no floating-point arguments)
-call printf     ; Call the printf function
-
-; Exit the program
-mov rax, 60     ; syscall number for exit
-xor rdi, rdi    ; Exit status 0
-syscall
+global main
+extern printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
