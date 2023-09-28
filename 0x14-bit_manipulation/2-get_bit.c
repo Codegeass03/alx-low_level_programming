8465b5918a8e03e32a217c3e5d2f4d0fd82236b9
Codegeass03 <vivianchepchumba32@gmail.com>
2023-09-28 04:30:59 -0700

    gt bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given index.
- * @n: searched number
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
  * @index: index of the bit
  *
- * Return: value of the bit at index index
- * or -1 if an error occured
+ * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-if (index <= 63)
-return (int n)
-((n >> index) && 1);
-else
+int bit_val;
+
+if (index > 63)
 return (-1);
+
+bit_val = (n >> index) & 1;
+
+return (bit_val);
 }
