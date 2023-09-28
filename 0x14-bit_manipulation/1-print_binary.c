f999884caef512f913137f7dab428eec4fced357
Codegeass03 <vivianchepchumba32@gmail.com>
2023-09-28 03:14:39 -0700

    get bit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 
 /**
  * print_binary -  prints the binary representation of a number.
- * @n: The number to be printed in binary
+ * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
